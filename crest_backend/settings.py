0a248c73a48a83fc403fdbe58bc057b1c1d37694
Anwasi Ayebaemi <ayewealth11@gmail.com>
2024-03-30 21:41:04 +0100

    Token

--- crest_backend/settings.py
+++ crest_backend/settings.py
@@ -198,7 +198,7 @@ CORS_ALLOWED_ORIGINS = [
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
-    "REFRESH_TOKEN_LIFETIME": timedelta(hours=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(day=1),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": False,
