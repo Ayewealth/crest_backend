b8d485cc875256bace5a42dba387668b23254997
Anwasi Ayebaemi <ayewealth11@gmail.com>
2024-06-19 17:35:02 +0100

    task 20hours

--- base/tasks.py
+++ base/tasks.py
@@ -7,8 +7,15 @@ logger = logging.getLogger(__name__)
 
 def daily_update_total_return():
     logger.info("Running daily update total return task")
-    subscriptions = InvestmentSubscription.objects.filter(
-        end_date__gte=timezone.now())
-    for subscription in subscriptions:
-        subscription.update_total_return()
-        logger.info(f"Updated total return for subscription {subscription.id}")
+    try:
+        subscriptions = InvestmentSubscription.objects.filter(
+            end_date__gte=timezone.now())
+        for subscription in subscriptions:
+            subscription.update_total_return()
+            logger.info(
+                f"Updated total return for subscription {subscription.id}")
+    except Exception as e:
+        logger.error(f"Error in daily_update_total_return: {e}")
+    finally:
+        from django.db import connection
+        connection.close()
